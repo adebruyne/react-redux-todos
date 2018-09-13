890d1770034fb118a19671c14654bb9c835a7408
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-13 18:35:13 -0400

    added function onSubmit and onChange

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,22 +5,55 @@ import { connect } from "react-redux";
 class TodoList extends Component {
   constructor(props) {
     super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      task: ""
+    };
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.dispatch({
+      type: "ADD_TODO",
+      task: this.state.task
+    });
+    this.setState({
+      task: ""
+    });
+  }
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
   render() {
-    let todos = [].map((task, index) => (
+    let todos = this.props.todos.map((task, index) => (
       <Todo task={task} key={index} />
     ));
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="task">Task</label>
+          <input
+            type="text"
+            name="task"
+            id="task"
+            onChange={this.handleChange}
+          />
+          <button>Add a Todo!</button>
+        </form>
+
         <ul>{todos}</ul>
       </div>
     );
   }
 }
-function mapStateToProps(reduxState){
-  return{
-
-  }
-};
+function mapStateToProps(reduxState) {
+  return {
+    todos: reduxState.todos
+  };
+}
 
 export default connect(mapStateToProps)(TodoList);
