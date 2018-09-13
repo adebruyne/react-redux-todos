599449f2ec846fd683697fd736c9c02e36edba1f
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-13 18:07:19 -0400

    add Provider to glue react-redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux";
+import { createStore } from "redux"; //Redux.createStore
 import rootReducer from "./rootReducer";
 import { Provider } from "react-redux";
 
@@ -12,5 +12,10 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
