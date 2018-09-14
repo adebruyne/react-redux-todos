84ef03042708898edd3080a117e8ec7bf7c388d9
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-14 16:36:35 -0400

    moved newform for todos into its own component

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,30 +1,19 @@
 import React, { Component } from "react";
 import Todo from "./Todo";
+import NewTodoForm from "./NewTodoForm";
 import { connect } from "react-redux";
 import { addTodo, removeTodo } from "./actionCreators";
+import { Route } from "react-router-dom";
 
 class TodoList extends Component {
   constructor(props) {
     super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-    this.state = {
-      task: ""
-    };
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+  handleAdd(val) {
+    this.props.addTodo(val);
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    this.props.addTodo(this.state.task);
-
-    event.target.reset();
-  }
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
   removeTodo(id) {
     // debugger;
     this.props.removeTodo(id);
@@ -40,18 +29,21 @@ class TodoList extends Component {
     ));
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="task">Task</label>
-          <input
-            type="text"
-            name="task"
-            id="task"
-            onChange={this.handleChange}
-          />
-          <button>Add a Todo!</button>
-        </form>
-
-        <ul>{todos}</ul>
+        <Route
+          path="/todos/new"
+          component={props => (
+            <NewTodoForm {...props} handleSubmit={this.handleAdd} />
+          )}
+        />
+        <Route
+          exact
+          path="/todos"
+          component={() => (
+            <div>
+              <ul>{todos}</ul>
+            </div>
+          )}
+        />
       </div>
     );
   }
