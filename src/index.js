32604b8b870dcd92d7d05d913217979ce6e73f60
Aylin DeBruyne <adebruyne88@gmail.com>
2018-09-18 08:54:20 -0400

    create functions for async calls to backend with ActionCreators

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore } from "redux"; //Redux.createStore
+import { createStore, applyMiddleware, compose } from "redux"; //Redux.createStore
 import rootReducer from "./rootReducer";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
@@ -11,7 +11,10 @@ import thunk from "redux-thunk";
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
